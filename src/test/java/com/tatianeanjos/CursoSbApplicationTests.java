c4e78d332ff606dc5b18a7b228ea77002cd04c58
Tatiane Anjos <tatianeanjos1997@gmail.com>
2020-07-09 11:09:11 -0300

    Criacao do projeto

---
+++ src/test/java/com/tatianeanjos/CursoSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tatianeanjos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
