54e90756eb7f8b0c1c3b5239656ac72b03f7a037
Tatiane Anjos <tatianeanjos1997@gmail.com>
2020-07-09 16:44:15 -0300

    Banco de dados H2 e criacao automatica de base de dados

--- src/main/java/com/tatianeanjos/domain/Categoria.java
+++ src/main/java/com/tatianeanjos/domain/Categoria.java
@@ -2,11 +2,19 @@ package com.tatianeanjos.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
 public class Categoria implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
+	@javax.persistence.Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer Id;
+	
 	private String nome;
 
 	public Categoria() {
