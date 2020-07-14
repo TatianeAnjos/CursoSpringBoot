9214bc731377df73b4cb075d7db27a33fbef69f2
Tatiane Anjos <tatianeanjos1997@gmail.com>
2020-07-14 12:11:55 -0300

    Class produto e associacao muitos para muitos

--- src/main/java/com/tatianeanjos/domain/Categoria.java
+++ src/main/java/com/tatianeanjos/domain/Categoria.java
@@ -1,21 +1,25 @@
 package com.tatianeanjos.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable{
-
 	private static final long serialVersionUID = 1L;
 	
 	@javax.persistence.Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer Id;
-	
 	private String nome;
+	
+	@ManyToMany(mappedBy = "categorias")
+	private List <Produto> produtos = new ArrayList();
 
 	public Categoria() {
 		
@@ -42,13 +46,20 @@ public class Categoria implements Serializable{
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
+	public List <Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List <Produto> produtos) {
+		this.produtos = produtos;
+	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
-		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
 		return result;
 	}
 
@@ -66,12 +77,8 @@ public class Categoria implements Serializable{
 				return false;
 		} else if (!Id.equals(other.Id))
 			return false;
-		if (nome == null) {
-			if (other.nome != null)
-				return false;
-		} else if (!nome.equals(other.nome))
-			return false;
 		return true;
 	}
-
+	
+	
 }
