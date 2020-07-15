1b97364b14bc2709984869c826e431179e58d8b5
Tatiane Anjos <tatianeanjos1997@gmail.com>
2020-07-15 12:18:32 -0300

    Cliente, Tipo cliente, Telefones e Enderecos

--- src/main/java/com/tatianeanjos/CursoSbApplication.java
+++ src/main/java/com/tatianeanjos/CursoSbApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.tatianeanjos.domain.Categoria;
 import com.tatianeanjos.domain.Cidade;
+import com.tatianeanjos.domain.Cliente;
+import com.tatianeanjos.domain.Endereco;
 import com.tatianeanjos.domain.Estado;
 import com.tatianeanjos.domain.Produto;
+import com.tatianeanjos.domain.enums.TipoCliente;
 import com.tatianeanjos.repositories.CategoriaRepository;
 import com.tatianeanjos.repositories.CidadeRepository;
+import com.tatianeanjos.repositories.ClienteRepository;
+import com.tatianeanjos.repositories.EnderecoRepository;
 import com.tatianeanjos.repositories.EstadoRepository;
 import com.tatianeanjos.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class CursoSbApplication implements CommandLineRunner{
 
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSbApplication.class, args);
@@ -52,6 +63,9 @@ public class CursoSbApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
 		Estado est1 = new Estado(null,"Minas Gerais");
 		Estado est2 = new Estado(null,"São Paulo");
 		
@@ -62,11 +76,20 @@ public class CursoSbApplication implements CommandLineRunner{
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2,c3));
 
-
-		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
-		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		
+		Cliente cli1 = new Cliente (null, "Maria Silva", "maria@gmail.com","123456789",TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("125124522","4581218484"));
+		
+		Endereco e1 = new Endereco(null, "Rua flores", "300", "Apto 303", "Jardim", "30502011", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "sala 8", "Centro", "0254620", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+		
 		
 	}
 
---
+++ src/main/java/com/tatianeanjos/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.tatianeanjos.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tatianeanjos.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente,Integer> {
+
+}
