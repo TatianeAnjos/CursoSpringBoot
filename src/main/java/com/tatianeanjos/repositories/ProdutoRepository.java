ca4a5051a66d4729c7ff1d0c9222b09e6766f574
Tatiane Anjos <tatianeanjos1997@gmail.com>
2020-07-30 16:52:51 -0300

    Subtotal e total do Pedido

--- src/main/java/com/tatianeanjos/repositories/ProdutoRepository.java
+++ src/main/java/com/tatianeanjos/repositories/ProdutoRepository.java
@@ -19,5 +19,6 @@ public interface ProdutoRepository extends JpaRepository<Produto,Integer> {
 	@Transactional(readOnly=true)
 	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
 	Page<Produto>search(@Param("nome") String nome,@Param("categorias") List<Categoria>categorias, Pageable pageRequest);
+	
 	//Método que busca sem a query: findDistinctByNomeContainingAndCategoriasIn
 }
