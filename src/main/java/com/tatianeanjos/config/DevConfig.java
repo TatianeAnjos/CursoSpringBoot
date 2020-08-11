9af0f1f68c50d466b314bd6d415bedfec8f71c89
Tatiane Anjos <tatianeanjos1997@gmail.com>
2020-08-11 20:26:36 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/tatianeanjos/config/DevConfig.java
+++ src/main/java/com/tatianeanjos/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.tatianeanjos.services.DBService;
+import com.tatianeanjos.services.EmailService;
+import com.tatianeanjos.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,5 +31,10 @@ public class DevConfig {
 
 		dbService.instatiateTesteDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
--- src/main/java/com/tatianeanjos/services/DBService.java
+++ src/main/java/com/tatianeanjos/services/DBService.java
@@ -122,7 +122,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		
-		Cliente cli1 = new Cliente (null, "Maria Silva", "maria@gmail.com","123456789",TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente (null, "Maria Silva", "tatianeanjos1997@gmail.com","123456789",TipoCliente.PESSOAFISICA);
 		cli1.getTelefones().addAll(Arrays.asList("125124522","4581218484"));
 		
 		Endereco e1 = new Endereco(null, "Rua flores", "300", "Apto 303", "Jardim", "30502011", cli1, c1);
